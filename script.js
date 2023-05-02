4f422fafa457a3694db1b6fb3cd1856832e50d12
ipadhim <ipadhim@qq.com>
2023-05-03 01:00:42 +0800

    Complete

---
+++ script.js
@@ -0,0 +1,14 @@
+const btn = document.getElementById("btn");
+const round = document.getElementById("round");
+btn.addEventListener(
+    "pointerdown",
+    (e) => {
+        round.style.setProperty("--l", e.pageX + "px");
+        round.style.setProperty("--t", e.pageY + "px");
+        setTimeout(() => {
+            round.style.transition = "clip-path 2s ease 0s";
+            btn.classList.add("active");
+        });
+    },
+    { once: true }
+);
