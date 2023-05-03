519632727ac6ab2dc1a98e6c82736d060794fac1
ipadhim <ipadhim@qq.com>
2023-05-03 10:47:30 +0800

    Update script.js

--- zoom-into-fullscreen/script.js
+++ zoom-into-fullscreen/script.js
@@ -1,14 +1,17 @@
 const btn = document.getElementById("btn");
 const round = document.getElementById("round");
 btn.addEventListener(
-    "pointerup",
+    "pointerdown",
     (e) => {
         round.style.setProperty("--l", e.pageX + "px");
         round.style.setProperty("--t", e.pageY + "px");
-        setTimeout(() => {
-            round.style.transition = "clip-path 2s ease 0s";
-            btn.classList.add("active");
-        });
+    }
+);
+btn.addEventListener(
+    "pointerup",
+    () => {
+       round.style.transition = "clip-path 2s ease 0s";
+       btn.classList.add("active");
     },
     { once: true }
 );
